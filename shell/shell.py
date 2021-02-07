0f83ff8659cfba102d73f4307723ab33f1bb3d24
Ken Amamori <kmamamori@miners.utep.edu>
2021-02-07 04:46:51 -0700

    Accomplished changing directory.

--- shell/shell.py
+++ shell/shell.py
@@ -1,11 +1,15 @@
-import os, sys
+import sys
+import os
 
 def main():
     while True:
-        p = os.getcwd()+' $' if 'PS1' not in os.environ else os.environ['PS1']
+        p = os.getcwd()+' $$$$' if 'PS1' not in os.environ else os.environ['PS1']
         os.write(1, p.encode())
         input = os.read(0, 128)
         input = input.decode()
+        input_splitted = input.split()
+        if input_splitted[0] == 'cd':
+            os.chdir(input_splitted[1])
         if "exit" in input:
             print("Exiting")
             sys.exit(1)
