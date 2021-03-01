38636b9aa972a16681dbf411eea3248eefe6e15d
kmamamori@miners.utep.edu <kmamamori@miners.utep.edu>
2021-02-28 18:54:23 -0700

    Completed part A of assignment2.

--- sockets_assignment1/client.py
+++ sockets_assignment1/client.py
@@ -4,8 +4,8 @@ import time
 s = socket(AF_INET, SOCK_STREAM)
 s.connect(("127.0.0.1", 7069))
 while 1:
-    data = s.recv(10000)
-    print(data.decode())
+    data = s.recv(10000).decode()
+    print(data)
     if len(data) == 0:
         break
 s.close()
---
+++ sockets_assignment2/client.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from socket import *
+
+
+#part A
+s = socket(AF_INET, SOCK_STREAM)
+s.connect(("nigelward.com", 80))
+#s.send("GET /index.html HTTP/1.0\r\n\r\n".encode())
+s.sendall(("GET /index.html HTTP/1.1\r\n" +
+    "Host: nigelward.com\r\n" +
+    "Accept: text/html\r\n" +
+    "Connection: close\r\n\r\n").encode())
+data = s.recv(1012).decode()
+data = s.recv(16).decode()
+print(data)
+#data = s.recv(16).decode()
+#print(data)
+s.close()
+
+#part B
