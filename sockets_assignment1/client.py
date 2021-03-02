f451b29fffb2834aa4b82391f6593d32c076b0a2
kmamamori@miners.utep.edu <kmamamori@miners.utep.edu>
2021-03-01 23:15:41 -0700

    Added deleted files in socket_assignment1

---
+++ sockets_assignment1/client.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from socket import *
+import time
+s = socket(AF_INET, SOCK_STREAM)
+s.connect(("127.0.0.1", 7069))
+while 1:
+    data = s.recv(10000)
+    print(data.decode())
+    if len(data) == 0:
+        break
+s.close()
