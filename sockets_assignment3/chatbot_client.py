50bdddd741f5f261f6aaad01ad2edee4e72c1b72
kmamamori <kmamamori@miners.utep.edu>
2021-03-11 00:31:24 -0700

    Updated socket_assignment3 server and client.

--- sockets_assignment3/chatbot_client.py
+++ sockets_assignment3/chatbot_client.py
@@ -2,126 +2,72 @@
 from socket import *
 import sys
 MLENGTH = 125
-
+ 
 class MySocket:
-    """demonstration class only
-      - coded for clarity, not efficiency
-    """
-
+ 
     def __init__(self, sock=None):
         if sock is None:
-            self.sock = socket.socket(
-                            socket.AF_INET, socket.SOCK_STREAM)
+            self.sock = socket(AF_INET, SOCK_STREAM)
         else:
             self.sock = sock
-
+ 
     def connect(self, host, port):
         self.sock.connect((host, port))
-
+ 
     def mysend(self, msg):
-        totalsent = 0
-        while totalsent < MLENGTH:
-            sent = self.sock.send(msg[totalsent:])
-            if sent == 0:
-                raise RuntimeError("socket connection broken")
-            totalsent = totalsent + sent
-
+        self.sock.send(msg.encode('utf-8'))
+ 
     def myreceive(self):
-        chunks = []
-        bytes_recd = 0
-        while bytes_recd < MLENGTH:
-            chunk = self.sock.recv(min(MLENGTH - bytes_recd, 2048))
-            if chunk == b'':
-                raise RuntimeError("socket connection broken")
-            chunks.append(chunk)
-            bytes_recd = bytes_recd + len(chunk)
-        return b''.join(chunks)
-
-    def myprepare(self,msg):
-        msg = 'X' +msg
+        chunk = self.sock.recv(MLENGTH)
+        chunk = chunk.strip()
+        return chunk
+ 
+    def myprepare(self,msg): # ensures that the message is of the right length
         while len(msg) < 100:
             msg = msg + ' '
         return msg
-
+ 
 class ConsoleReader:
     def __init__(self):
         pass
     
     def get_next_line(self):
         return input()
-
+ 
 class FileReader:
     def  __init__(self, filename):
         with open(filename) as f:
             content = f.readlines()
         self.file_lines = [x.strip() for x in content] 
-
+ 
     def get_next_line(self):
         return self.file_lines.pop(0)
-
-#def send_message(message): 
-#   totalsent = 0
-#    while totalsent < MLENGTH:
-#        sent = s.send(message[totalsent:])
-#        if sent == 0:
-#            raise RuntimeError("socket connection broken")
-#        totalsent = totalsent + sent
-
-#def recieve_message():
-#    chunks = []
-#    bytes_recd =0
-
- # probably here recieve the message length and then recieve the rest of the stuff
-#    while bytes_recd <MLENGTH:
-#        chunk = s.recv(min(MLENGTH-bytes_recd, 2048))
-#        if chunk ==b'':
-#            raise RuntimeError("socket connection broken")
-#        chunks.append(chunk)
-#        bytes_recd = bytes_recd +len(chunk)
-#    return b''.join(chunks)
-
-#def prepare_message(message):
-#   message = 'X' +message
-#    while len(message) < 100:
-#        message = message + ' '
-#    return message
-
+ 
 def main():
     try:
         start_socket()
     except FileNotFoundError:
         print("File was not found")
-    except IndexError:
-        print("File has not enought lines to conversate")
-    except BaseException:
-        print("No valid arguments")
-
+ 
 def start_socket():
     s = MySocket()
     s.connect("localhost",7069)
     reader = get_reader()
     conversate(s, reader)
-
+ 
 def get_reader():
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 1:
         return ConsoleReader()
-    
-        return FileReader(sys.argv[1])    
-    raise BaseException
-
-
+    else:
+        return FileReader(sys.argv[1])
+ 
 def conversate(s, reader):
+    response = s.myreceive()
+    print(response.decode())
     while True:
         currentline = reader.get_next_line()
         currentline = s.myprepare(currentline)
-        s.send_message(currentline)
         s.mysend(currentline)
         response = s.myreceive()
-        print(response.decode)
-    
-
-
-
-
-
+        print(response.decode())
 main()
