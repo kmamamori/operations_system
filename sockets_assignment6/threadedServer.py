e7c32edad0e062616aa9cbf90b1eb06f24f57743
kmamamori <kmamamori@miners.utep.edu>
2021-03-22 21:51:07 -0600

    Completed locks.

--- sockets_assignment6/threadedServer.py
+++ sockets_assignment6/threadedServer.py
@@ -9,14 +9,16 @@ def clientHandler(clientSocket, address, threadNumber):
     print("I'm a new thread, number %d" % threadNumber)
     print("  handling communications with " , address)
     for x in range(1,5):
+        lck.acquire()
         oldCumulativeResponses = cumulativeResponses
-        time.sleep(5)
+        time.sleep(2)
         time1 = time.time()
         nowtime = datetime.datetime.now()
         toSendString = "hello from " + gethostname() + nowtime.strftime(" %A %I:%M")
         toSendBytes = toSendString.encode()
         clientSocket.send(toSendBytes)
         cumulativeResponses = oldCumulativeResponses + 1
+        lck.release()
         myResponses = myResponses + 1
         print("Thread %d has done %d sends; all threads %d" %
               (threadNumber,myResponses, cumulativeResponses))
@@ -28,6 +30,7 @@ def clientHandler(clientSocket, address, threadNumber):
 nThreads = 0 
 global cumulativeResponses
 cumulativeResponses = 0
+lck = _thread.allocate_lock()
 s = socket(AF_INET, SOCK_STREAM)
 s.bind(("localhost", 7069))
 s.listen(5)
